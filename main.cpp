9b4fc02500807312d7bf0788131a89644a9f5540
Katarzyna Adamczyk <kasia.cholost@gmail.com>
2020-09-24 13:45:10 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -7,17 +7,18 @@ unsigned int add_digits_2(unsigned int liczba);
 
 int main()
 {
-    cout << "Witaj w programie add digits " << endl;
+    cout << "Welcome to add digits console app" << endl;
     unsigned int n {1};
     while (n>0)
     {
-        cout<<"Podaj liczbe, dla ktorej mam policzyc sume cyfr."<<endl;
-        cout<<"Jesli podasz 0 to konczymy gre."<<endl;
+        cout << "Please put in a number for which I will count the sum of the digits repeatedly \n"
+                "until I get a single number."<<endl;
+        cout<<"If you put in 0 the program will end."<<endl;
         cin>>n;
         unsigned int wynik = add_digits(n);
-        cout<<"Wynikiem algorytmu 1 jest: "<<wynik<<endl;
+        cout<<"For algorithm 1 the sum is: "<<wynik<<endl;
         wynik = add_digits_2(n);
-        cout<<"Wynikiem algorytmu 2 jest: "<<wynik<<endl;
+        cout<<"For algorithm 2 the sum is: "<<wynik<<endl;
         cout<<endl<<endl;
     }
     return 0;
